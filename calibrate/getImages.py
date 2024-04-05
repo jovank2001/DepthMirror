e16d78a1c64fba18808695b9b817d8b79757f422
jovank2001 <jovank2001@gmail.com>
2024-04-05 16:19:45 -0700

    picamera2

--- calibrate/getImages.py
+++ calibrate/getImages.py
@@ -9,50 +9,25 @@ Created: April 5, 2024
 Version: 1.0
 License: MIT License
 """
-
+from picamera2 import Picamera2, Preview
+import time
 import cv2 as cv
 
 #Settings
-resolution = [640, 480] #[Width, Height]
+resolution = (640, 480) #(Width, Height)
 fps = 20
 wait = (1/fps)*1000 #Wait in milliseconds between frames
 
-#Create camera objects and set resolution
-capR = cv.VideoCapture(0)
-capL = cv.VideoCapture(1)
-capR.set(cv.CAP_PROP_FRAME_WIDTH, resolution[0])
-capR.set(cv.CAP_PROP_FRAME_HEIGHT, resolution[1])
-capL.set(cv.CAP_PROP_FRAME_WIDTH, resolution[0])
-capL.set(cv.CAP_PROP_FRAME_HEIGHT, resolution[1])
+#Create camera objects, ensure 'unpacked' data format and set resolution
+camR = Picamera2(0)
+camL = Picamera2(1)
+config = camR.create_preview_configuration(sensor={'format':'SRGGB8', 'size': resolution})
+camR.configure(config)
+camL.configure(config)
+print(camR.preview_configuration.sensor) #Verify settings
+print(camL.preview_configuration.sensor) #Verify settings
 
 #Capture Images
-num = 0 # Index for image capture loop below
-while capR.isOpened() and capL.isOpened():
-
-    successR, imgR = capR.read()
-    successL, imgL = capL.read()
-
-    #Wait 50ms before displaying next frame 
-    k = cv.waitKey(wait) 
-
-    if k == ord('q'): #Quit on 'q' keypress
-        break
-    elif k == ord('s'): #Take photo on 's' keypress
-        cv.imwrite('calibrate/images/rightCam/capR' + str(num) + '.png', imgR)
-        cv.imwrite('calibrate/images/leftCam/capL' + str(num) + '.png', imgL)
-        print("Pics taken" + str(num))
-        num += 1
-
-    cv.imshow("Right", imgR)
-    cv.imshow("Left", imgL)
-
-capR.release()
-capL.release()
-
-cv.destroyAllWindows()
-
-
-        
 
 
 
