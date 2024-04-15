89cc98eff4e47ab3df52182b4fb20bb34ca9607b
jovank2001 <jovank2001@gmail.com>
2024-04-14 18:45:11 -0700

    Fixed bug

--- calibrate/getImages.py
+++ calibrate/getImages.py
@@ -22,7 +22,12 @@ wait = (1/fps)*1000 #Wait in milliseconds between frames
 #Create camera objects, ensure 'unpacked' data format and set resolution
 camR = Picamera2(0)
 camL = Picamera2(1)
-config = camR.create_still_configuration(raw={'format':'SRGGB8','size': resolution})
+configR = camR.create_still_configuration(raw={'format':'SRGGB8','size': resolution})
+configL = camL.create_still_configuration(raw={'format':'SRGGB8','size': resolution})
+camR.configure(configR)
+camL.configure(configL)
+
+#Display settings
 pprint(camR.sensor_modes)
 print(camR.preview_configuration.raw) #Verify settings
 print(camL.preview_configuration.raw) #Verify settings
@@ -33,14 +38,14 @@ camL.start(show_preview = True)
 numPics = 5
 imageCount = 0
 
-#Get the images every 3 seconds
+#Get the images every keypress
 while imageCount < numPics:
 
     input("Click Enter to capture pics")
-    fPathR = "calibrate/images/rightCam/imgR"+str(imageCount)+".jpg"
-    fPathL = "calibrate/images/leftCam/imgL"+str(imageCount)+".jpg"
-    camR.capture_file(config, fPathR)
-    camL.capture_file(config, fPathL)
+    fPathR = "/calibrate/images/rightCam/imgR"+str(imageCount)+".jpg"
+    fPathL = "/calibrate/images/leftCam/imgL"+str(imageCount)+".jpg"
+    camR.capture_file(fPathR)
+    camL.capture_file(fPathL)
     print("Images captured")
     imageCount += 1
 
