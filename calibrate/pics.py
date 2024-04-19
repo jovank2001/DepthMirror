279caad7b04a9e9dc867860f3ab3ae703d986147
Jovan Koledin <jovank2001@gmail.com>
2024-04-19 12:37:22 -0700

    Better calibration pics

--- calibrate/pics.py
+++ calibrate/pics.py
@@ -16,13 +16,13 @@ from pprint import *
 def takePics():
         
     #Settings
-    resolution = (640, 480) #(Width, Height)
+    resolution = (1280, 960) #(Width, Height)
 
     #Create camera objects, ensure 'unpacked' data format and set resolution
     camR = Picamera2(0)
     camL = Picamera2(1)
-    configR = camR.create_preview_configuration(raw={'format':'SRGGB8','size': resolution})
-    configL = camL.create_preview_configuration(raw={'format':'SRGGB8','size': resolution})
+    configR = camR.create_preview_configuration(raw={'format':'SRGGB8', 'size':resolution})
+    configL = camL.create_preview_configuration(raw={'format':'SRGGB8', 'size':resolution})
     camR.configure(configR)
     camL.configure(configL)
 
@@ -33,15 +33,15 @@ def takePics():
     #Capture images after key press numPics times
     camL.start(show_preview = True)
     camR.start(show_preview = True)
-    numPics = 5
+    numPics = 15
     imageCount = 0
 
     #Get the images every keypress
     while imageCount < numPics:
 
         input("Click Enter to capture pics")
-        fPathR = "images/rightCam/imgTestR"+str(imageCount)+".jpg"
-        fPathL = "images/leftCam/imgTestL"+str(imageCount)+".jpg"
+        fPathR = "images/rightCam/imgR"+str(imageCount)+".jpg"
+        fPathL = "images/leftCam/imgL"+str(imageCount)+".jpg"
         camR.capture_file(fPathR)
         camL.capture_file(fPathL)
         print("Images captured")
